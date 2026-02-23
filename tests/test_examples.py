cb15152820290f3c8341f06a27f79192272b74bb
Nicholas Car <nick@kurrawong.ai>
2026-02-23 14:22:35 +1000

    5.3 - ValPub valid

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,6 +6,7 @@ EG_DIR = Path(__file__).parent.parent.resolve() / "examples"
 VALIDATORS_DIR = Path(__file__).parent.parent.resolve() / "validators"
 
 def test_examples():
+    print()
     for f in sorted(list(EG_DIR.glob("*.ttl"))):
         validity = False if "invalid" in str(f.name) else True
 
@@ -14,7 +15,7 @@ def test_examples():
         elif "multilang" in str(f.name):
             validator = VALIDATORS_DIR / "validator.multilang.ttl"
         else:
-            validator = VALIDATORS_DIR / "validator.ttl"
+            validator = VALIDATORS_DIR / "vocpub.ttl"
 
         data_g = Graph().parse(f)
         validator_g = Graph().parse(validator)
@@ -23,4 +24,5 @@ def test_examples():
             validator_g.parse(VALIDATORS_DIR / "expander.ttl")
 
         v = validate(data_g, shacl_graph=validator_g, allow_warnings=True, advanced=True)
-        assert v[0] == validity, print(f.name)
+        assert v[0] == validity, print(f"{f.name} is not correct: {v[2]}")
+        print(f"{f.name} is correct")
